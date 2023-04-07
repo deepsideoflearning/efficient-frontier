45b038c8a21509ed4a26c1273fe17e3022b2f191
Christopher Himmel <chris.himmel@deepsideoflearning.com>
2023-04-07 17:21:27 -0400

    Add files via upload

---
+++ app.py
@@ -0,0 +1,33 @@
+from PIL import Image
+import streamlit as st
+from constants import *
+import datetime
+
+
+if __name__=='__main__':
+    st.header('Efficient Frontier')
+
+    st.sidebar.title('Navigation')
+
+    ct = datetime.datetime.now()
+    st.sidebar.write("Current time:", ct)
+
+    coin_choice = st.sidebar.text_input("Coin:", "BTC")
+
+    # Get Data
+
+    endpoint = 'https://min-api.cryptocompare.com/data/histoday'
+    res = requests.get(endpoint + '?fsym='+coin_choice+'&tsym=USD&limit='+str(days))
+    hist = pd.DataFrame(json.loads(res.content)['Data'])
+
+    hist = hist.set_index('time')
+    hist.index = pd.to_datetime(hist.index, unit='s')
+    pd.DatetimeIndex(hist['date']).month==1
+
+    hist.drop(["conversionType", "conversionSymbol"], axis = 'columns', inplace = True)
+    
+    hist_year = hist[pd.DatetimeIndex(hist['date']).month*pd.DatetimeIndex(hist['date']).day==1]
+    
+    st.header(coin_choice +' daily activity')
+    st.write(hist_year.sort_values(by=['time'], ascending=False))
+    
