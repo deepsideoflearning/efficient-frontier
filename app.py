75f160f74d826f152e84e3f464cea9291d3ac3cb
Christopher Himmel <chris.himmel@deepsideoflearning.com>
2023-04-07 17:31:00 -0400

    Add files via upload

--- app.py
+++ app.py
@@ -20,7 +20,7 @@ if __name__=='__main__':
     # Get Data
 
     endpoint = 'https://min-api.cryptocompare.com/data/histoday'
-    res = requests.get(endpoint + '?fsym='+coin_choice+'&tsym=USD&limit=2000')
+    res = requests.get(endpoint + '?fsym='+coin_choice+'&tsym=USD&limit=4000')
     hist = pd.DataFrame(json.loads(res.content)['Data'])
 
     hist = hist.set_index('time')
@@ -30,6 +30,7 @@ if __name__=='__main__':
     hist.drop(["conversionType", "conversionSymbol"], axis = 'columns', inplace = True)
     
     hist_year = hist[pd.DatetimeIndex(hist['date']).month*pd.DatetimeIndex(hist['date']).day==1]
+    hist_year.drop(["date"])
     
     st.header(coin_choice +' daily activity')
     st.write(hist_year.sort_values(by=['time'], ascending=False))
